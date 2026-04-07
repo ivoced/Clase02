e243935eb8a50a1b67a483f1ccdf00d3aa3ae3a7
ivoced <ivo.cedron@neo.com.pe>
2026-04-07 10:05:47 -0500

    Nuevo frontend

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.servicios import router as servicios_router
 from routes.auth import router as auth_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluir routers
 app.include_router(servicios_router)
